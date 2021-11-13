7c6fc5dd30e9d9fba5a5a5f841858a9c1ecea681
jmconn22 <59665851+MyNameBeJohn@users.noreply.github.com>
2021-11-13 16:59:50 -0500

    Bye bye components directory

--- components/Logic.js
+++
@@ -1,6 +0,0 @@
-export function divMoney(){
-    const dict_money = Math.floor((Math.random()*1001) + 1); 
-    const offer_money = Math.floor(Math.random()*(dict_money - 1) + 1); 
-
-    return [offer_money, dict_money]; 
-}
