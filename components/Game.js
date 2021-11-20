6360e61c680f72779dca2a89bbc47ad44f13e3ae
jmconn22 <59665851+MyNameBeJohn@users.noreply.github.com>
2021-11-19 22:07:16 -0500

    Updated Game component
    Now has a way of uploading files to s3 with the correct name and data

--- components/Game.js
+++ components/Game.js
@@ -1,6 +1,10 @@
 import React from "react";
 import {divMoney} from "./Logic";
-import { useState } from "react";
+import { uploadFile } from 'react-s3';
+import AWS from 'aws-sdk'
+const BrowserFS = require('browserfs');
+
+//Source: https://javascript.plainenglish.io/how-to-create-download-and-upload-files-in-react-apps-80893da4247a
 
 const t_styles = {
     width: '300px',
@@ -12,7 +16,7 @@ const p_styles ={
     width: "300px",
     margin: "10px auto",
     textAlign: 'center', 
-}
+};
 
 const h_style = {
     background:" #aa0420",
@@ -23,7 +27,7 @@ const h_style = {
     width: "15%",
     textAlign: 'center',
     margin: '20px auto',
-}
+};
 
 const b_style ={
     background:"#bb3420",
@@ -34,22 +38,22 @@ const b_style ={
     width: "15%",
     textAlign: 'center',
     margin: '20px auto',
-}
+};
 
 const display_style={
     textAlign: 'center',
     margin: '20px auto',
-}
+};
 
 const background = {
     background:"#cc99ff", 
     padding: "5px"
-}
+};
 
 const pa_styles = {
     textAlign: 'center',  
     padding: "10px",
-}
+};
 
 const [offered_money, dict_money] = divMoney(); 
 
@@ -57,9 +61,42 @@ export default class Game extends React.Component {
 
     constructor(){
         super(); 
-        this.state = {value:""};
+        this.state = {fileDownloadUrl: null, value:"", money: offered_money};
         this.onChange = this.onChange.bind(this); 
         this.add = this.add.bind(this); 
+
+        this.download = this.download.bind(this);
+        this.fileName = "dictator_" + Math.floor(Math.random()*10000000000)
+
+        this.config1 = {
+            bucketName: 'bigbucketboyz',
+            region: 'us-east-2',
+            accessKeyId: 'AKIAVGCSL7QH5KFDTDET',
+            secretAccessKey: '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',
+        }
+
+        this.config2 = {
+            bucketName: "dictdestinationbucket",
+            region:"us-east-2",
+            accessKeyId: 'AKIAVGCSL7QH5KFDTDET',
+            secretAccessKey: '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',
+        }
+
+        this.s3 = new AWS.S3({params: {Bucket:'bigbucketboyz'}, region:"us-east-2", accessKeyId:'AKIAVGCSL7QH5KFDTDET', 
+        secretAccessKey:'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'})
+
+        this.promise = new Promise((resolve, reject) => {
+        })
+  }
+
+    handleUpload(file, file2, config, params){
+        uploadFile(file, config)
+        .then(() => (uploadFile(file2,config))
+        .then(() => (this.s3.copyObject(params, function(err, data){
+            if(err) console.log(err, err.stack);
+            else    console.log(data);})))      
+        .catch(err => console.error(err))) 
+
     }
 
     add() {
@@ -82,6 +119,52 @@ export default class Game extends React.Component {
 
     refreshPage(){
         window.location.reload(false);
+    }
+
+    makeCSV(input){
+        let csv = ''; 
+
+        input.forEach(value =>{
+            value.forEach((item, i) => {
+                let innerVal = item === null ? "" : item.toString();
+                let result = innerVal.replace(/"/g, '""');
+                if (result.search(/("|,|\n)/g) >= 0){
+                    result = '"' + result + '"'
+                }
+                if (i > 0) {csv += ','}
+                csv += result;
+                })
+                csv += '\n'; 
+            })
+        return csv; 
+    }
+
+    
+    download(event){
+        event.preventDefault();
+
+        let output;
+        let contents = []; 
+        contents.push(["desc, "+this.state.value, "offer_money, "+this.state.money]); 
+
+        output = this.makeCSV(contents);
+
+        const blob = new Blob([output]);
+        console.log(output)
+
+        const file = {
+            name:this.fileName,
+        }
+
+        const params = {
+            Bucket: "dictdestinationbucket",
+            CopySource: "dictdestinationbucket/undefined",
+            Key: this.fileName
+        }  
+
+        this.handleUpload(blob, file, this.config2, params)
+
+        this.add(); 
     }
 
     render () { 
@@ -107,9 +190,10 @@ export default class Game extends React.Component {
                 </tbody> 
             </table>                
             </form>
-            <div className="add-item" style = {t_styles}>
+            <div style = {t_styles}>
                 <input type="text" className="add-item__input" onChange={this.onChange} placeholder={"Enter yes or no"}/>
-                <button disabled={!this.state.value} className="add-item__button" onClick={this.add}>Submit</button>
+                <button disabled={!this.state.value} onClick={this.download}>Submit</button>
+                <a className="hidden" download={'dictator.csv'} href={this.state.fileDownloadUrl} ref={e=>this.dofileDownload = e}></a>
             </div>
             {this.state.value === "YES_STATE"?
                 <div style={pa_styles}>
